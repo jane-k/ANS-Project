25a520e2b2acb287550ceaa19391ac2583a1d9fb
chanmin, kim <kimchanmin1@gmail.com>
2022-03-25 00:40:04 +0900

    feat: implement store mutate logic

--- store/ansData/index.js
+++ store/ansData/index.js
@@ -1,7 +1,10 @@
 export const state = () => ({
   selectedANSType: null,
+  // 계산이 완료된 ANS 데이터 결과물
   ANSData: null,
+  // 계산용 초기 데이터베이스
   initialData: null,
   filteredANSData: null,
-  filteredDataType: null
+  filteredDataName: null,
+  filteredDataCount: 0
 })
